0288d717777f32be8dd05806b4e3d2422a12375f
Jhai Navarro <jhai@freelancer.com>
2020-06-04 04:33:00 +0800

    Show Draw status if ever no one won

--- src/index.js
+++ src/index.js
@@ -115,6 +115,8 @@ class Game extends React.Component {
     let status;
     if (winner) {
       status = `Winner: ${winner}`;
+    } else if (this.state.move === 9) {
+      status = 'Draw';
     } else {
       status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
     }
